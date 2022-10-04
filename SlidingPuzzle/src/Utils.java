7476f24144f170cecf36b54bbb1f121de6783c49
dmelendr <114924869+dmelendr@users.noreply.github.com>
2022-10-04 16:48:34 +0200

    Board print arreglado

--- SlidingPuzzle/src/Utils.java
+++ SlidingPuzzle/src/Utils.java
@@ -3,14 +3,14 @@ public class Utils {
 	
 	private static int SIZE=4;
 	
-	public void printBoard(int[][] board) {
-		System.out.println("---------------");
+	public static void printBoard(int[][] board) {
+		System.out.println("---------------------------------");
 		for(int contX=0; contX< SIZE;contX++) {
 			for(int contY=0; contY< SIZE;contY++) {
 				System.out.print("| "+board[contX][contY]+" \t");
 			}
-			System.out.print("|");
+			System.out.print("|\n");
 		}
-		System.out.println("---------------");
+		System.out.println("---------------------------------");
 	}
 }
