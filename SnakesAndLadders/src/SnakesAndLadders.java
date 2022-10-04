41ba8b3c265c701b70476b963de561d040aa8802
jconchel <jaime.conchellobauto@emeal.nttdata.com>
2022-10-04 15:58:46 +0200

    juego

--- SnakesAndLadders/src/SnakesAndLadders.java
+++ SnakesAndLadders/src/SnakesAndLadders.java
@@ -14,26 +14,33 @@ public class SnakesAndLadders {
 
 		int move = 1;
 		boolean gameOver = false;
-
+		
 		while (!gameOver) {
+			int dice1 = Utils.rollDice();
+			int dice2 = Utils.rollDice();
+			System.out.println("Dados1: " + dice1 );
+			System.out.println("Dados2: " + dice2 );
+			
 			if (move % 2 == 1) {
 				// player 1 hace cosas
-				player1.setPosition(board.calculatePosition(player1.getPosition() + Utils.rollDice()));
+				player1.setPosition(board.calculatePosition(player1.getPosition() + dice1 + dice2));
 				System.out.println("Player1 - " + player1.getPosition() );
 				gameOver = (player1.getPosition() == 100);
 			}
 
 			if (move % 2 == 0) {
 				// player 2 hace cosas
-				player2.setPosition(board.calculatePosition(player2.getPosition() + Utils.rollDice()));
+				player2.setPosition(board.calculatePosition(player2.getPosition() + dice1 + dice2));
 				System.out.println("Player2 - " + player2.getPosition() );
 				gameOver = (player2.getPosition() == 100);
 			}
 
-			move++;
+			if(dice1 != dice2) move++;
 
 		}
 		// Comprobar ganador
+		if(player1.getPosition() == 100) System.out.println("Player1 ha ganado" );
+		else System.out.println("Player2 ha ganado" );
 
 	}
 	
