7988197ba03fc4c3005e5bbcabeb974fc7f57f92
Alvaro Martin Martin <alvmarti@emeal.nttdata.com>
2022-10-04 15:09:44 +0200

    BUCLE DEL JUEGO

--- SnakesAndLadders/src/SnakesAndLadders.java
+++ SnakesAndLadders/src/SnakesAndLadders.java
@@ -1,15 +1,29 @@
 public class SnakesAndLadders {
-	
-	 public static void main(final String[] args) {
-		    System.out.println("MAIN");
-		    
-		    Player player1 = new Player();
-		    player1.setPosition(0);
-		    
-		    Player player2 = new Player();
-		    player2.setPosition(0);
-		    
-		    
-		  }
+
+	public static void main(final String[] args) {
+		System.out.println("MAIN");
+
+		Player player1 = new Player();
+		player1.setPosition(0);
+
+		Player player2 = new Player();
+		player2.setPosition(0);
+
+		int move = 1;
+
+		while (true) {
+			if (move % 2 == 1) {
+				// player 1 hace cosas
+			}
+
+			if (move % 2 == 0) {
+				// player 2 hace cosas
+			}
+
+			move++;
+
+		}
+
+	}
 
 }
