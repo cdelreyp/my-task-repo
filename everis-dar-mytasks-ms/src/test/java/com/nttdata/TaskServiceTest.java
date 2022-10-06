6248a180149410936ffc5c29809f485a0aceba88
jconchel <jaime.conchellobauto@emeal.nttdata.com>
2022-10-06 17:20:11 +0200

    Import update

--- everis-dar-mytasks-ms/src/test/java/com/nttdata/TaskServiceTest.java
+++ everis-dar-mytasks-ms/src/test/java/com/nttdata/TaskServiceTest.java
@@ -21,6 +21,9 @@ import org.junit.jupiter.api.TestMethodOrder;
 import com.nttdata.repository.TaskRepository;
 import com.nttdata.repository.entities.Task;
 import com.nttdata.service.TaskService;
+import com.nttdata.service.dto.TaskAddDTO;
+import com.nttdata.service.dto.TaskDTO;
+import com.nttdata.service.dto.TaskUpdateDTO;
 import com.nttdata.utils.enums.enumStatus;
 
 
