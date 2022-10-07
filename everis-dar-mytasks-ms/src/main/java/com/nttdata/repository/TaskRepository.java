a43c4096a3989b2a6b04a09870653bd925086cf2
Alvaro Martin Martin <alvmarti@emeal.nttdata.com>
2022-10-07 10:37:36 +0200

    Service Interface

--- everis-dar-mytasks-ms/src/main/java/com/nttdata/repository/TaskRepository.java
+++ everis-dar-mytasks-ms/src/main/java/com/nttdata/repository/TaskRepository.java
@@ -1,17 +1,11 @@
 package com.nttdata.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.nttdata.repository.entities.Task;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long>{
-	
-	/*@Query(value = "from Tasks where status != 3")
-	List<Task> findAllActive();*/
+public interface TaskRepository extends JpaRepository<Task, Long> {
 
 }
--- everis-dar-mytasks-ms/src/main/java/com/nttdata/service/TaskService.java
+++ everis-dar-mytasks-ms/src/main/java/com/nttdata/service/TaskService.java
@@ -1,116 +1,28 @@
 package com.nttdata.service;
 
-import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.nttdata.repository.TaskRepository;
 import com.nttdata.repository.entities.Task;
 import com.nttdata.service.dto.TaskAddDTO;
 import com.nttdata.service.dto.TaskDTO;
 import com.nttdata.service.dto.TaskUpdateDTO;
-import com.nttdata.service.mappers.TaskMapper;
 import com.nttdata.utils.enums.enumStatus;
 
 @Service
-public class TaskService {
+public interface TaskService {
 
-	@Autowired
-	private TaskRepository taskRepository;
+	ArrayList<TaskDTO> getAllTasks();
 
-	@Autowired
-	private TaskMapper taskMapper;
+	ArrayList<TaskDTO> getAllByStatus(enumStatus status);
 
-	public ArrayList<TaskDTO> getAllTasks() {
+	Task getTaskById(Long id);
 
-		List<Task> tasks = taskRepository.findAll();
+	Task addTask(TaskAddDTO task);
 
-		ArrayList<TaskDTO> tasksDTO = (ArrayList<TaskDTO>) taskMapper.getAllTasksMapper(tasks);
+	Task updateTask(TaskUpdateDTO task, Long id);
 
-		return tasksDTO;
-	}
+	void deleteTask(Long id);
 
-	public ArrayList<TaskDTO> getAllByStatus(enumStatus status) {
-
-		List<Task> tasks = taskRepository.findAll().stream().filter(p -> p.getStatus() == status)
-				.collect(Collectors.toList());
-
-		ArrayList<TaskDTO> tasksDTO = (ArrayList<TaskDTO>) taskMapper.getAllTasksMapper(tasks);
-
-		return tasksDTO;
-	}
-
-	public Task getTaskById(Long id) {
-
-		Optional<Task> task = taskRepository.findById(id);
-
-		if (task.isPresent()) {
-
-			return task.get();
-		}
-
-		return null;
-	}
-
-	public Task addTask(TaskAddDTO task) {
-
-		Task taskToAdd = new Task();
-
-		taskToAdd.setDescription(task.getDescription());
-		taskToAdd.setStatus(enumStatus.IN_PROGRESS);
-
-		taskToAdd.setEntry_date(new Timestamp(System.currentTimeMillis()));
-		taskToAdd.setModified_date(taskToAdd.getEntry_date());
-
-		taskRepository.save(taskToAdd);
-
-		return taskToAdd;
-	}
-
-	public Task updateTask(TaskUpdateDTO task, Long id) {
-
-		Optional<Task> taskToUpdate = taskRepository.findById(id);
-
-		if (taskToUpdate.isPresent() &&  !taskToUpdate.get().getStatus().equals(enumStatus.DELETED)) {
-
-			if (task.getDescription() != null) {
-				taskToUpdate.get().setDescription(task.getDescription());
-				taskToUpdate.get().setModified_date(new Timestamp(System.currentTimeMillis()));
-			}
-			if (task.getStatus() != null) {
-				taskToUpdate.get().setStatus(task.getStatus());
-				taskToUpdate.get().setModified_date(new Timestamp(System.currentTimeMillis()));
-			}
-
-			taskRepository.save(taskToUpdate.get());
-
-			return taskToUpdate.get();
-		}
-
-		return null;
-	}
-
-	public void deleteTask(Long id) {
-
-		Optional<Task> task = taskRepository.findById(id);
-
-		if (task.isPresent()) {
-
-			Task taskToDelete = task.get();
-
-			if (taskToDelete.getStatus() != enumStatus.DELETED) {
-				taskToDelete.setStatus(enumStatus.DELETED);
-				taskToDelete.setCancel_date(new Timestamp(System.currentTimeMillis()));
-
-				taskRepository.save(taskToDelete);
-			}
-
-		}
-
-	}
 }
